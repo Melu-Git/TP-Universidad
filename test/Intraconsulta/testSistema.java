ed19ec04e329416d3c7f6e1bca5c86145bdcc4c6
Melina <violamelina.b@gmail.com>
2023-09-10 18:05:50 -0300

    primer commit

---
+++ test/Intraconsulta/testSistema.java
@@ -0,0 +1,13 @@
+package Intraconsulta;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class testSistema {
+	
+	@Test
+	public void queSePuedaRegistrarUnAlumno() {
+
+		}
+	
+	
+}
